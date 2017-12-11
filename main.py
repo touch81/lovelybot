54af913b880cda0b92950f322ac8c47ea0573742
touch81 <34262598+touch81@users.noreply.github.com>
2017-12-11 21:47:42 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from microsoftbotframework import MsBot
 from tasks import *
 import os
 
-    bot = MsBot(port=int(os.environ['PORT']))
-    bot = MsBot()
-    bot.add_process(echo_response)
-    bot.run()
-    bot.run(port=int(os.environ['PORT']))
+#bot = MsBot(port=int(os.environ['PORT']))
+bot = MsBot()
+bot.add_process(echo_response)
+#bot.run()
+bot.run(port=int(os.environ['PORT']))
